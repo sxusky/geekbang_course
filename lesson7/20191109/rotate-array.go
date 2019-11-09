90c33f7931dd5d9c31cfa0183ae12272977945a0
sxusky <sxusky@gmail.com>
2019-11-09 23:59:35 +0800

    rotate-array

--- lesson7/20191109/rotate-array.go
+++ lesson7/20191109/rotate-array.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func rotate(nums []int, k int) {
+	// 拼接为两倍数组，k求mod，然后从len的位置往左挪kMod位，然后切片len个元素
 	numsLen := len(nums)
 	kMod := k % numsLen
 	doubleNums := append(nums, nums...)
-	nums = doubleNums[numsLen-kMod : numsLen*2-kMod]
+	nums = doubleNums[numsLen-kMod : numsLen-kMod+numsLen]
 	fmt.Println("nums:", nums)
 }
 
@@ -17,4 +18,7 @@ func main() {
 	rotate(nums, 3)
 	nums = []int{-1, -100, 3, 99}
 	rotate(nums, 2)
+	rotate(nums, 0)
+	rotate(nums, 1)
+	rotate(nums, 4)
 }
